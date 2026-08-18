3fd76f09b29787257f621606e25526be3cb3b998
munizcesar@live.com <147671167+munizcesar@users.noreply.github.com>
2026-08-18 19:19:45 -0300

    fix(build): corrige tipagem do DOMMatrix e paths conflitantes com pdf-parse no type-check do next

--- src/app/api/mentor/generate/route.ts
+++ src/app/api/mentor/generate/route.ts
@@ -56,11 +56,15 @@ export async function POST(req: Request) {
     if (file) {
       try {
         if (typeof global !== 'undefined') {
+          // @ts-ignore
           if (typeof global.DOMMatrix === 'undefined') {
-            global.DOMMatrix = class DOMMatrix { constructor() { this.a=1; this.b=0; this.c=0; this.d=1; this.e=0; this.f=0; } };
+            // @ts-ignore
+            global.DOMMatrix = class DOMMatrix { constructor() { this.a=1; this.b=0; this.c=0; this.d=1; this.e=0; this.f=0; } } as any;
           }
-          if (typeof global.Path2D === 'undefined') global.Path2D = class Path2D {};
-          if (typeof global.ImageData === 'undefined') global.ImageData = class ImageData {};
+          // @ts-ignore
+          if (typeof global.Path2D === 'undefined') global.Path2D = class Path2D {} as any;
+          // @ts-ignore
+          if (typeof global.ImageData === 'undefined') global.ImageData = class ImageData {} as any;
         }
 
         const pdfParse = require("pdf-parse");
